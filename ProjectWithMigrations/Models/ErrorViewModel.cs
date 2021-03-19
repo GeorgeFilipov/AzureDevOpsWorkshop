939e18f9d145d63b2a325f5512aaa55771362ea3
Georgi Filipov <georgi.filipov@uplink.bg>
2021-03-19 11:13:17 +0200

    Add project files.

---
+++ ProjectWithMigrations/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ProjectWithMigrations.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
